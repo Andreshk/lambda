08247598ca151a4550959f60d2c2f78dd0d359a2
Andrey Drenski <andreshko.dre@gmail.com>
2018-09-02 13:05:50 +0300

    initial commit

---
+++ lcpt.hs
@@ -0,0 +1,9 @@
+module LCPT where
+import Lambda(Lambda(..), pretty, i, k, ks, s, w, om, y, j,
+              valid, beta, betaStep, betaSteps, betaSteps_)
+import NLambda
+import W
+
+main = putStrLn $
+  "This module serves as to unify the exported functionality \n\
+  \of the other modules. For example usage, see README.md"
