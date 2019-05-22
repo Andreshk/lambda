845060279191af79b5242b375cec521565bec6bb
Andreshk <andreshko.dre@gmail.com>
2019-05-23 01:12:30 +0300

    encode to / decode from BitVector

--- binary.hs
+++ binary.hs
@@ -1,49 +1,69 @@
 module Binary (encode,decode) where
 import Control.Applicative ((<|>))
+import Data.BitVector (BitVector, ones, zeros, (#), fromBits, size,
+                       msb1, complement, least, (!.), (==.), nil)
 import Lambda (Lambda(..),i,k,ks,s,w,om,y,j)
 
 -- A variable with DeBruijn index n is encoded to n+1 ones and a zero
 -- (10,110,1...10). Abstraction is encoded to 00, and application to 01.
-encode :: Lambda -> String
-encode (Var n) = replicate (n+1) '1' ++ ['0']
-encode (Ap ts) = foldl1 (\x y -> "01" ++ x ++ y) (map encode ts)
-encode (L k t) = replicate (2*k) '0' ++ encode t
+encode :: Lambda -> BitVector
+encode (Var n) = ones (n+1) # zeros 1
+encode (Ap ts) = foldl1 (\x y -> (fromBits [False,True]) # x # y) (map encode ts)
+encode (L k t) = zeros (2*k) # encode t
 
--- Decodes (if possible) a variable, and returns it with the rest of the encoded string
-decVar :: String -> Maybe (Lambda, String)
-decVar str
+-- Get the number of leading bits of the corresponding type
+leading :: Char -> BitVector -> Int
+leading '0' bv = (size bv - msb1 bv - 1)
+leading '1' bv = (size bv - msb1 bv' - 1)
+  where bv' = complement bv -- to-do: don't flip the entire bitvector
+
+-- Drop the most-significant k bits from a bitvector
+dropBV :: Int -> BitVector -> BitVector
+dropBV k bv
+  | rest <= 0 = nil
+  | otherwise = least rest bv
+  where rest = (size bv - k)
+
+-- Decodes (if possible) a variable, and returns it with the rest of the encoded bitvector
+decVar :: BitVector -> Maybe (Lambda, BitVector)
+decVar bv
   | ones == 0 = Nothing
   | otherwise = Just (Var (ones-1), rest)
-  where ones = length $ takeWhile (=='1') str
-        rest = drop (ones+1) str
+  where ones = leading '1' bv
+        rest = dropBV (ones+1) bv
 
 -- Decodes a repeated abstraction
-decL :: String -> Maybe (Lambda, String)
-decL str
+decL :: BitVector -> Maybe (Lambda, BitVector)
+decL bv
   | k == 0    = Nothing
   | otherwise = do (t,rest) <- decTerm body; return (L k t, rest)
-  where k = (`div`2) . length $ takeWhile (=='0') str
-        body = drop (2*k) str
+  where k = (leading '0' bv) `div` 2
+        body = dropBV (2*k) bv
 
 -- Decodes an application, and compresses the returned term (if possible)
-decAp :: String -> Maybe (Lambda, String)
-decAp ('0':'1':str) = do
-    (m,temp) <- decTerm str
-    (n,rest) <- decTerm temp
-    case m of Ap ts -> Just (Ap (ts++[n]),rest)
-              _     -> Just (Ap [m,n], rest)
-decAp _ = Nothing
+decAp :: BitVector -> Maybe (Lambda, BitVector)
+decAp bv
+  | prefixedBy01 = decAp' $ dropBV 2 bv
+  | otherwise    = Nothing
+  where prefixedBy01 = size bv >= 2 && not (bv !. 0) && bv !. 1
+        decAp' bv = do
+          (m,temp) <- decTerm bv
+          (n,rest) <- decTerm temp
+          case m of Ap ts -> Just (Ap (ts++[n]),rest)
+                    _     -> Just (Ap [m,n], rest)
 
 -- Decoding a term in this system is straight-forward
-decTerm :: String -> Maybe (Lambda, String)
-decTerm str = (decVar str)
-          <|> (decAp str)
-          <|> (decL str)
+decTerm :: BitVector -> Maybe (Lambda, BitVector)
+decTerm bv = (decVar bv)
+         <|> (decAp bv)
+         <|> (decL bv)
 
 -- The user-facing decoding function needs to decode the entire
--- given string, not just some prefix (with garbage afterwards).
-decode :: String -> Maybe Lambda
-decode str = do (t,"") <- decTerm str; return t
+-- given bitvector, not just some prefix (with garbage afterwards).
+decode :: BitVector -> Maybe Lambda
+decode str = do
+  (t,rest) <- decTerm str
+  if rest ==. nil then Just t else Nothing
 
 -- Check whether decoding an encoded term gives the same term
 checkTerm :: Lambda -> Bool
