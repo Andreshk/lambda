41655773c2047d8a49eebbe5f89c8d84a80ddf71
Andrey Drenski <andreshko.dre@gmail.com>
2019-08-15 11:21:53 +0300

    make Lambda a separate class & add alpha-equivalence and validity checks
    This required some finicking with std::visit & std::get_if, but is a far out better decision than `using Lambda = std::variant<...>`

--- Lambda.cpp
+++ Lambda.cpp
@@ -1,34 +1,36 @@
 ﻿#include "Lambda.h"
-#include <algorithm>
 
-Ap::Ap(std::vector<Lambda>&& terms) : terms{} {
-	if (Ap* t0 = std::get_if<Ap>(&terms[0]); t0) {
-		this->terms = std::move(t0->terms);
-		for (size_t i = 1; i < terms.size(); ++i) {
-			this->terms.emplace_back(std::move(terms[i]));
+Ap::Ap(std::initializer_list<Lambda> terms) : terms{} {
+	// The terms are "compressed" - a term will contain all of its arguments in the same vector.
+	// This means that ((xy)z) will be repesented as a single node (xyz), and it should be
+	// impossible for the first term (the "function" applied) to be an application itself.
+	if (const Ap* t0 = Lambda::get_if<Ap>(terms.begin()); t0) {
+		this->terms = t0->terms;
+		for (auto it = terms.begin() + 1; it != terms.end(); ++it) {
+			this->terms.emplace_back(std::move(*it));
 		}
 	} else {
-		this->terms = std::move(terms);
+		this->terms = terms;
 	}
+	vassert(this->terms.size() >= 2);
 }
 
-Ap::Ap(std::initializer_list<Lambda> terms) : Ap(std::vector(terms)) {} // not-so-uniform initialization
-
-L::L(unsigned k, Lambda&& term) : k{ k } {
+L::L(const unsigned k, Lambda&& body) : k{ k } {
+	// Consecutive abstractions \x\y.(...) should also be compressed into a single node: \xy.(...)
 	vassert(k > 0 && "There should be at least one bound variable!");
-	if (L* m1 = std::get_if<L>(&term); m1) {
-		this->k += m1->k;
-		this->term = std::make_unique<Lambda>(std::move(*m1));
+	if (L* term1 = Lambda::get_if<L>(&body); term1) {
+		this->k += term1->k;
+		this->body = std::move(term1->body);
 	} else {
-		this->term = std::make_unique<Lambda>(std::move(term));
+		this->body = std::make_unique<Lambda>(std::move(body));
 	}
 }
 
 // Prints a generic name from the list: u,v,w,x,y,z,u0,v0,w0,...
-void printGenericName(std::ostream& os, const unsigned i) {
-	os << char('u' + i % 6);
-	if (i >= 6) {
-		os << (i / 6) - 1;
+void printGenericName(std::ostream& os, const unsigned idx) {
+	os << char('u' + idx % 6);
+	if (idx >= 6) {
+		os << (idx / 6) - 1;
 	}
 }
 
@@ -44,7 +46,7 @@ public:
 	}
 	// Print the mapped name of a given variable.
 	void printName(std::ostream& os, const unsigned idx) const {
-		// There term may have free variables - for them the context has no mapping
+		// The term may have free variables - for them the context has no mapping
 		if (idx < n) {
 			// We "flip" the index due to the reverse order of the numbers
 			printGenericName(os, n - 1 - idx);
@@ -60,6 +62,7 @@ public:
 	void pop(const unsigned k) {
 		n -= k;
 	}
+	// Current size of the context
 	unsigned size() const {
 		return n;
 	}
@@ -69,15 +72,14 @@ public:
 // implicit context. For named contexts pushing and popping expand and contract the visible "space"
 // of names. Of course, once the given names run out, the implicit context behaviour will kick in.
 class NamedContext : public ImplicitContext {
-	const std::vector<std::string>& names;
+	const std::vector<std::string_view>& names;
 public:
-	NamedContext(const std::vector<std::string>& names) : ImplicitContext{}, names { names } {}
+	NamedContext(const std::vector<std::string_view>& names) : ImplicitContext{}, names { names } {}
 	// Print the mapped name of a given variable.
 	void printName(std::ostream& os, const unsigned idx) const {
 		// We remap only the outermost variables (the ones with the highest DeBruijn indices).
-		// This means the highest indices should map to indices [0..] in the names array.
-		const unsigned n = ImplicitContext::size();
-		const unsigned idx1 = n - 1 - idx;
+		// This means the highest indices should map in reverse to indices [0..] in the names array.
+		const unsigned idx1 = size() - 1 - idx;
 		if (idx1 < names.size()) {
 			os << names[idx1];
 		} else {
@@ -86,6 +88,8 @@ public:
 	}
 };
 
+// Visitor for pretty-printing a lambda term to a given output stream,
+// parameterized by the context that's used to generate names for variable indices.
 template <class Context>
 struct PrintVisitor {
 	std::ostream& os;
@@ -93,40 +97,113 @@ struct PrintVisitor {
 
 	PrintVisitor(std::ostream& os, Context& ctx) : os{ os }, ctx{ ctx } {}
 	// The visiting functions:
-	void operator()(const Var& t) {
-		ctx.printName(os, t.idx);
+	void operator()(const Var& l) {
+		ctx.printName(os, l.idx);
 	}
-	void operator()(const Ap& t) {
-		for (const auto& term : t.terms) {
+	void operator()(const Ap& l) {
+		for (const auto& term : l.terms) {
 			// Put brackets only around the complex terms
-			const bool needsBr = !std::get_if<Var>(&term);
+			const bool needsBr = !Lambda::get_if<Var>(&term);
 			if (needsBr) { os << '('; }
-			std::visit(*this, term); // Use the same visitor, to preserve the context
+			Lambda::visit(*this, term); // Use the same visitor, to preserve the context
 			if (needsBr) { os << ')'; }
 		}
 	}
-	void operator()(const L& t) {
+	void operator()(const L& l) {
 		// Push the names of the new k bound variables into the context
-		ctx.push(t.k);
+		ctx.push(l.k);
 		os << '\\';
-		for (unsigned k = t.k; k > 0; --k) {
+		for (unsigned k = l.k; k > 0; --k) {
 			ctx.printName(os, k - 1);
 		}
 		os << '.';
-		std::visit(*this, *t.term);
+		Lambda::visit(*this, *l.body);
 		// It is obligatory to pop the new names from the context afterwards.
-		ctx.pop(t.k);
+		ctx.pop(l.k);
 	}
 };
 
 std::ostream& operator<<(std::ostream& os, const Lambda& l) {
 	ImplicitContext ctx{};
-	std::visit(PrintVisitor{ os,ctx }, l);
+	Lambda::visit(PrintVisitor{ os,ctx }, l);
 	return os;
 }
 
-void printWithCtx(std::ostream& os, const Lambda& l, const std::vector<std::string>& names) {
+void printWithCtx(std::ostream& os, const Lambda& l, const std::vector<std::string_view>& names) {
 	NamedContext ctx{ names };
-	std::visit(PrintVisitor{ os,ctx }, l);
+	Lambda::visit(PrintVisitor{ os,ctx }, l);
 	os << '\n';
+}
+
+// Visitor class for alpha-equivalence. Used for traversing two lambda terms in
+// parallel, checking whether they have the same structure and the same content.
+struct EquivVisitor {
+	bool operator()(const Var& l1, const Var& l2) const {
+		return (l1.idx == l2.idx);
+	}
+	bool operator()(const Ap& l1, const Ap& l2) const {
+		if (l1.terms.size() != l2.terms.size()) {
+			return false;
+		}
+		for (size_t i = 0; i < l1.terms.size(); ++i) {
+			if (!Lambda::visit(*this, l1.terms[i], l2.terms[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+	bool operator()(const L& l1, const L& l2) const {
+		return (l1.k == l2.k && Lambda::visit(*this, *l1.body, *l2.body));
+	}
+	// Catch-all clause (much more verbose than [](auto x, auto y){ ... })
+	// for when std::visit does not find a better-matching clause.
+	template <typename V1, typename V2>
+	bool operator()(const V1&, const V2&) const {
+		return false;
+	}
+};
+
+bool operator==(const Lambda& l1, const Lambda& l2) {
+	return Lambda::visit(EquivVisitor{}, l1, l2);
+}
+bool operator!=(const Lambda& l1, const Lambda& l2) {
+	return !(l1 == l2);
+}
+
+// Normal reduction strategy, corresponding to lazy evaluation.
+bool Lambda::betaStep() {
+	vassert(false && "Not implemented!");
+	return false;
+}
+
+unsigned Lambda::betaReduce() {
+	unsigned numRegexes = 0;
+	// Each beta-step reduces one regex.
+	while (betaStep()) {
+		++numRegexes;
+	}
+	return numRegexes;
+}
+
+bool Lambda::isValid() const {
+	struct ValidVisitor {
+		bool operator()(const Var& l) {
+			return true; // A variable is always structurally valid
+		}
+		bool operator()(const Ap& l) {
+			if (l.terms.size() < 2 || Lambda::get_if<Ap>(&l.terms[0])) {
+				return false;
+			}
+			for (const Lambda& term : l.terms) {
+				if (!Lambda::visit(*this, term)) {
+					return false;
+				}
+			}
+			return true;
+		}
+		bool operator()(const L& l) {
+			return (l.k > 0 && !Lambda::get_if<L>(l.body.get()) && Lambda::visit(*this, *l.body));
+		}
+	};
+	return Lambda::visit(ValidVisitor{}, *this);
 }
--- Lambda.h
+++ Lambda.h
@@ -2,31 +2,76 @@
 #include <iostream>
 #include <variant>
 #include <vector>
-#include <string>
+#include <string_view>
 
 #include <cassert>
 #define vassert assert
 
-using Lambda = std::variant<struct Var, struct Ap, struct L>;
+// Forward declaration
+class Lambda;
 
+// A lambda term is either:
+// 1. A variable, denoted by its Debruijn index
 struct Var {
 	unsigned idx;
 };
-
+// 2. An application of a term and one or more arguments (all kept together in a vector)
 struct Ap {
 	std::vector<Lambda> terms;
-	Ap(std::vector<Lambda>&& terms);
 	Ap(std::initializer_list<Lambda> terms);
 };
-
+// 3. An abstraction, binding one or more variables
 struct L {
 	unsigned k;
-	std::unique_ptr<Lambda> term;
-	L(unsigned k, Lambda&& term);
-	// A copy ctor is required
-	L(const L& other) : k{ other.k }, term{ other.term ? std::make_unique<Lambda>(*other.term) : nullptr } {}
+	std::unique_ptr<Lambda> body;
+	L(unsigned k, Lambda&& body);
+	// A copy ctor and operator= are required
+	L(const L& other) : k{ other.k }, body{ std::make_unique<Lambda>(*other.body) } {}
+	L& operator=(const L& other) {
+		if (this != &other) {
+			this->~L();
+			new (this) L(other);
+		}
+		return *this;
+	}
 };
 
+// We can now define a lambda as a union of the three elementary types.
+class Lambda : std::variant<Var, Ap, L> {
+	using Base = std::variant<Var, Ap, L>;
+	using Base::Base; // Use all of std::variant's ctors and assignment operators
+	using Base::operator=;
+public:
+	// std::visit is SFINAE-restricted to specializations of std::variant -> so we make our own visit
+	template <class Visitor, class... Lambdas>
+	static decltype(auto) visit(Visitor&& v, const Lambdas& ...ls) {
+		return std::visit(std::forward<Visitor>(v), reinterpret_cast<const Base&>(ls)...);
+	}
+	// The private inheritance bars the conversion from Lambda* to Base* -> so we make our own get_if, too
+	template <typename T>
+	static T* get_if(Lambda* l) { return std::get_if<T>(reinterpret_cast<Base*>(l)); }
+	template <typename T>
+	static const T* get_if(const Lambda* l) { return std::get_if<T>(reinterpret_cast<const Base*>(l)); }
+
+private:
+	// Perform one beta-reduction step using the normal reduction strategy
+	// (reduce the outermost, left-most redex). Returns true if no regex is found.
+	bool betaStep();
+public:
+	// Beta reduction. Currently does not recognize terms that reduce to themselves.
+	// Returns the number of steps needed (i.e. the number of reduced regexes).
+	unsigned betaReduce();
+
+	// Check whether the term is properly compressed (see Ap::Ap() and L::L())
+	bool isValid() const;
+};
+
+// Pretty-print a given lambda term, using automatically generated names.
 std::ostream& operator<<(std::ostream& os, const Lambda& l);
 
-void printWithCtx(std::ostream& os, const Lambda& l, const std::vector<std::string>& names);
+// Pretty-print a given lambda term, using the given names for the outermost bound variables.
+void printWithCtx(std::ostream& os, const Lambda& l, const std::vector<std::string_view>& names);
+
+// Lambda term alpha-equivalence
+bool operator==(const Lambda& l1, const Lambda& l2);
+bool operator!=(const Lambda& l1, const Lambda& l2);
